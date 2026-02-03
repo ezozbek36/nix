cdae1d4fedce791a576fd9fef33ae03dd8a19dcd
Ezozbek <git@ezozbek.dev>
2026-02-03 21:04:04 +0500

    refactor nix config

--- modules/nixos/nix-settings.nix
+++ modules/nixos/nix-settings.nix
@@ -5,8 +5,12 @@ _: {
     experimental-features = ["nix-command" "flakes"];
     auto-optimise-store = true;
 
-    substituters = ["https://attic.xuyh0120.win/lantian"];
-    trusted-public-keys = ["lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="];
+    substituters = ["https://nix-community.cachix.org" "https://cuda-maintainers.cachix.org" "https://attic.xuyh0120.win/lantian"];
+    trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+      "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
+    ];
   };
 
   nixpkgs.config.allowUnfree = true;
