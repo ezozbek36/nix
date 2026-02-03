17c1695a5f67cda94b9a5228c1c91e3e234e1d95
Ezozbek <git@ezozbek.dev>
2026-02-03 11:20:55 +0500

    update kernel params

--- modules/nixos/boot.nix
+++ modules/nixos/boot.nix
@@ -21,7 +21,9 @@
     kernel.sysctl = {
       "vm.dirty_ratio" = 10;
       "vm.dirty_background_ratio" = 5;
-      "vm.swappiness" = 10;
+      "vm.swappiness" = 180;
+      "vm.watermark_boost_factor" = 0;
+      "vm.page-cluster" = 0;
       "net.core.default_qdisc" = "fq";
       "net.ipv4.tcp_congestion_control" = "bbr";
     };
