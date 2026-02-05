61036150cb4d9c527fd8ee8a286ffeeded88c52a
Ezozbek <git@ezozbek.dev>
2026-02-05 06:32:23 +0500

    fix alacritty build

--- overlays/alacritty.nix
+++ overlays/alacritty.nix
@@ -3,7 +3,7 @@ final: prev: {
     env =
       (oldAttrs.env or {})
       // {
-        RUSTFLAGS = "-C target-cpu=x86-64-v3 -C lto=fat -C codegen-units=1 -C panic=abort";
+        RUSTFLAGS = "-C target-cpu=x86-64-v3 -C lto=fat -C codegen-units=1";
       };
   });
 }
