e22a5e24e836761c104b0c83bffa9f4e4e268519
Ezozbek <git@ezozbek.dev>
2026-02-05 07:19:29 +0500

    use mold linker alacritty

--- overlays/alacritty.nix
+++ overlays/alacritty.nix
@@ -1,9 +1,11 @@
 final: prev: {
   alacritty = prev.alacritty.overrideAttrs (oldAttrs: {
+    nativeBuildInputs = (oldAttrs.nativeBuildInputs or []) ++ [final.mold];
+
     env =
       (oldAttrs.env or {})
       // {
-        RUSTFLAGS = "-C target-cpu=x86-64-v3 -C lto=fat -C codegen-units=1";
+        RUSTFLAGS = "-C target-cpu=x86-64-v3 -C lto=fat -C codegen-units=1 -C link-arg=-fuse-ld=mold";
       };
   });
 }
