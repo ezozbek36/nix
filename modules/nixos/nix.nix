42eb63f2e38153092f3b73a8c5760397584ae383
Ezozbek <git@ezozbek.dev>
2026-04-20 08:02:06 +0500

    use lix

--- modules/nixos/nix.nix
+++ modules/nixos/nix.nix
@@ -1,15 +1,19 @@
-_: {
-  nix.settings = {
-    trusted-users = ["root" "ezozbek"];
+{pkgs, ...}: {
+  nix = {
+    package = pkgs.lixPackageSets.stable.lix;
 
-    experimental-features = ["nix-command" "flakes"];
-    auto-optimise-store = true;
+    settings = {
+      trusted-users = ["root" "ezozbek"];
 
-    substituters = ["https://nix-community.cachix.org" "https://cuda-maintainers.cachix.org" "https://attic.xuyh0120.win/lantian"];
-    trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
-      "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
-    ];
+      experimental-features = ["nix-command" "flakes"];
+      auto-optimise-store = true;
+
+      substituters = ["https://nix-community.cachix.org" "https://cuda-maintainers.cachix.org" "https://attic.xuyh0120.win/lantian"];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+        "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
+      ];
+    };
   };
 }
