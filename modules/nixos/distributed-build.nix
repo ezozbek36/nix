ead268f973b47ce659d8ffa9b055211376bf14b8
Ezozbek <git@ezozbek.dev>
2026-04-20 16:33:01 +0500

    use build substitutes

--- modules/nixos/distributed-build.nix
+++ modules/nixos/distributed-build.nix
@@ -1,9 +1,13 @@
 {...}: {
   nix = {
     distributedBuilds = true;
+    settings = {
+      builders-use-substitutes = true;
+    };
     buildMachines = [
       {
         maxJobs = 100;
+        protocol = "ssh-ng";
         system = "x86_64-linux";
         hostName = "eu.nixbuild.net";
         supportedFeatures = ["benchmark" "big-parallel"];
